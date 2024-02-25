1904bb451567a5d453c6a6e26461bfb96394ee1a
Nishchal <nischal.15gupta@gmail.com>
2024-02-25 19:55:18 +0530

    starter files

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+app.listen(port, () => {
+    console.log(`Backend server running at http://${host}:${port}`);
+})
