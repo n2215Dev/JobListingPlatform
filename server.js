8480edb3291534ed3e519ec3b62c93e05a646399
Nishchal <nischal.15gupta@gmail.com>
2024-02-25 20:09:24 +0530

    changes made in server.js file

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
 app.listen(port, () => {
-    console.log(`Backend server running at http://${host}:${port}`);
+    console.log(`Backend server running successfully at http://${host}:${port}`);
 })
