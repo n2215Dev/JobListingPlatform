eb21e0e98b713b2b41aa8591f2d1af9e1fa1f3df
Nishchal <nischal.15gupta@gmail.com>
2024-02-29 18:17:36 +0530

    modified

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
+console.log("localhost", host);
+
+app.get('/health', (req, res) => {
+    res.json({
+        service: "Job Listing Backend API Server",
+        status: "true",
+        time: new Date(),
+    });
+});
+
 app.listen(port, () => {
     console.log(`Backend server running successfully at http://${host}:${port}`);
 })
